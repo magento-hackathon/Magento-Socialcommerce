ee1fd35c87b2ece2b105e648e0c62cf021f8eee7
rmp <rmp8427@gmail.com>
2012-10-27 14:28:02 +0200

    Added composite for adapter

---
+++ app/code/community/Hackathon/Socialcommerce/Model/AdapterComposite.php
@@ -0,0 +1,20 @@
+<?php
+
+class Hackathon_Socialcommerce_Model_AdapterComposite
+{
+
+    protected $_childs = array();
+
+    function add ( Hackathon_Socialcommerce_Model_Adapter_Interface $object )
+    {
+        $hash                 = spl_object_hash($object);
+        $this->_childs[$hash] = $object;
+    }
+
+    function sendSinglePost(Hackathon_Socialcommerce_Model_SinglePost $post) {
+        /** @var $child Hackathon_Socialcommerce_Model_Adapter_Interface */
+        foreach ($this->_childs as $child) {
+            $child->sendSinglePost($post);
+        }
+    }
+}
