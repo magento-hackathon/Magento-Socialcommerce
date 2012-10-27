8b6e2623e84b4e0c891d20d099f6f78b7f484634
rmp <rmp8427@gmail.com>
2012-10-27 15:27:21 +0200

    Some documentation

--- src/app/code/community/Hackathon/Socialcommerce/Model/Adapter/Interface.php
+++ src/app/code/community/Hackathon/Socialcommerce/Model/Adapter/Interface.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * Interface for every adapter
+ */
 interface Hackathon_Socialcommerce_Model_Adapter_Interface
 {
     public function sendSinglePost(Hackathon_Socialcommerce_Model_Messagetype_SinglePost $post);
