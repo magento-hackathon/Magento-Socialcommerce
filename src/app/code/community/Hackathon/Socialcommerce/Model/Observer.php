719fd78bd81ec8f1e95c829b3b311241706b2ba3
Thorsten Reiser <reiser@flagbit.de>
2012-10-27 21:55:08 +0200

    [~TASK] cleaned up observer

--- src/app/code/community/Hackathon/Socialcommerce/Model/Observer.php
+++ src/app/code/community/Hackathon/Socialcommerce/Model/Observer.php
@@ -12,19 +12,18 @@ class Hackathon_Socialcommerce_Model_Observer extends Hackathon_Socialcommerce_M
         /** @var $product Mage_Catalog_Model_Product */
         $product = $observer->getEvent()->getProduct();
 
-        Hackathon_Socialcommerce_Log::log("Fetched before saving product ID=" . $product->getId());
-
-        if (null == $product->getId()) {
-            // no id: product is new
-
-            /** @var $post Hackathon_Socialcommerce_Model_Messagetype_Singlepost */
-            $post = Mage::getModel('socialcommerce/messagetype_singlepost');
-            $post->setText($this->_getConfig()->getMessageNewProduct());
-            $post->importProduct($product);
-
-            /** @var $twitter Hackathon_Socialcommerce_Model_Adapter_Twitter */
-            $twitter = Mage::getModel('socialcommerce/adapter_twitter');
-            $twitter->sendSinglePost($post);
+        if (null != $product->getId()) {
+            return false;
         }
+        // no id: product is new
+
+        /** @var $post Hackathon_Socialcommerce_Model_Messagetype_Singlepost */
+        $post = Mage::getModel('socialcommerce/messagetype_singlepost');
+        $post->setText($this->_getConfig()->getMessageNewProduct());
+        $post->importProduct($product);
+
+        /** @var $twitter Hackathon_Socialcommerce_Model_Adapter_Twitter */
+        $twitter = Mage::getModel('socialcommerce/adapter_twitter');
+        $twitter->sendSinglePost($post);
     }
 }
