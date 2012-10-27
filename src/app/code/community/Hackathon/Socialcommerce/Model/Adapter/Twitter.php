66da2b01ef36ea7a89b86774e27762d711aaf606
rmp <rmp8427@gmail.com>
2012-10-27 15:51:06 +0200

    Extract method Model::Adapter::Twitter::_getClient()

--- src/app/code/community/Hackathon/Socialcommerce/Model/Adapter/Twitter.php
+++ src/app/code/community/Hackathon/Socialcommerce/Model/Adapter/Twitter.php
@@ -8,26 +8,47 @@ class Hackathon_Socialcommerce_Model_Adapter_Twitter extends Hackathon_Socialcom
 {
 
     /**
+     * @var Zend_Service_Twitter
+     */
+    protected $_client;
+
+    /**
      * Send a single post to twitter
      *
      * @param Hackathon_Socialcommerce_Model_Messagetype_SinglePost $post
      */
     public function sendSinglePost ( Hackathon_Socialcommerce_Model_Messagetype_SinglePost $post )
     {
-        $access = new Zend_Oauth_Token_Access();
-        $access->setToken($this->_getConfig()->getTwitterAuthToken())
-            ->setTokenSecret($this->_getConfig()->getTwitterTokenSecret());
 
-        $params = array(
-            'accessToken'    => $this->_getConfig()->getTwitterAccessToken(),
-            'consumerKey'    => $this->_getConfig()->getTwitterConsumerKey(),
-            'consumerSecret' => $this->_getConfig()->getTwitterConsumerSecret(),
-        );
 
-        $twitter = new Zend_Service_Twitter( $params );
-
-        $twitter->statusUpdate($post->getText());
+        $this->_getClient()->statusUpdate($post->getText());
         // @todo picture
         // @todo shorten url
+    }
+
+    /**
+     * Get the twitter client
+     *
+     * @return Zend_Service_Twitter
+     */
+    protected function _getClient ()
+    {
+
+        if ( null == $this->_client )
+        {
+            $access = new Zend_Oauth_Token_Access();
+            $access->setToken($this->_getConfig()->getTwitterAuthToken())
+                ->setTokenSecret($this->_getConfig()->getTwitterTokenSecret());
+
+            $params = array(
+                'accessToken'    => $this->_getConfig()->getTwitterAccessToken(),
+                'consumerKey'    => $this->_getConfig()->getTwitterConsumerKey(),
+                'consumerSecret' => $this->_getConfig()->getTwitterConsumerSecret(),
+            );
+
+            $this->_client = new Zend_Service_Twitter( $params );
+        }
+
+        return $this->_client;
     }
 }
