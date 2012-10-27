57513a567e7d6659e019769b525ad6ece6e9f648
rmp <rmp8427@gmail.com>
2012-10-27 16:47:10 +0200

    Assert that composite has all methods

--- src/app/code/community/Hackathon/Socialcommerce/Model/AdapterComposite.php
+++ src/app/code/community/Hackathon/Socialcommerce/Model/AdapterComposite.php
@@ -1,6 +1,6 @@
 <?php
 
-class Hackathon_Socialcommerce_Model_AdapterComposite
+class Hackathon_Socialcommerce_Model_AdapterComposite implements Hackathon_Social_Model_Adapter_Interface
 {
 
     protected $_childs = array();
@@ -11,9 +11,11 @@ class Hackathon_Socialcommerce_Model_AdapterComposite
         $this->_childs[$hash] = $object;
     }
 
-    function sendSinglePost(Hackathon_Socialcommerce_Model_Messagetype_Singlepost $post) {
+    function sendSinglePost ( Hackathon_Socialcommerce_Model_Messagetype_Singlepost $post )
+    {
         /** @var $child Hackathon_Socialcommerce_Model_Adapter_Interface */
-        foreach ($this->_childs as $child) {
+        foreach ( $this->_childs as $child )
+        {
             $child->sendSinglePost($post);
         }
     }
