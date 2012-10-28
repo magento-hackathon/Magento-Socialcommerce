d05a0e6925bfdf146b21d343b3e640ac8e112de2
rmp <rmp8427@gmail.com>
2012-10-28 14:13:34 +0100

    Fetch event sales_convert_quote_to_order for publish new message (e.g. "Another satisfied Customer")

--- src/app/code/community/Hackathon/Socialcommerce/Model/Observer.php
+++ src/app/code/community/Hackathon/Socialcommerce/Model/Observer.php
@@ -26,4 +26,18 @@ class Hackathon_Socialcommerce_Model_Observer extends Hackathon_Socialcommerce_M
         $twitter = Mage::getModel('socialcommerce/adapter_twitter');
         $twitter->sendSinglePost($post);
     }
+
+
+    /**
+     * Event: sales_convert_quote_to_order
+     */
+    public function onSalesConvertQuoteToOrder(Varien_Event_Observer $observer ) {
+        /** @var $post Hackathon_Socialcommerce_Model_Messagetype_Singlepost */
+        $post = Mage::getModel('socialcommerce/messagetype_singlepost');
+        $post->setText($this->_getConfig()->getMessageNewOrder());
+
+        /** @var $twitter Hackathon_Socialcommerce_Model_Adapter_Twitter */
+        $twitter = Mage::getModel('socialcommerce/adapter_twitter');
+        $twitter->sendSinglePost($post);
+    }
 }
